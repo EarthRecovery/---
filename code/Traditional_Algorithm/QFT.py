3e9a2e68c65b269c34d1c77c3484110f97ea538a
EarthRecovery <ssyyl35@nottingham.edu.cn>
2023-01-25 21:19:53 +0800

    qft improve

--- code/Traditional_Algorithm/QFT.py
+++ code/Traditional_Algorithm/QFT.py
@@ -1,9 +1,14 @@
 from unittest import result
 import numpy as np
 import cirq
+import matplotlib.pyplot as plt
 
 def main():
-    qft_circuit = generate_2x2_grid_qft_circuit()
+    a,b,c,d = [cirq.GridQubit(0,0),
+              cirq.GridQubit(0,1),
+              cirq.GridQubit(1,1),
+              cirq.GridQubit(1,0),]
+    qft_circuit = generate_2x2_grid_qft_circuit(a,b,c,d)
     print('Circuit:')
     print(qft_circuit)
 
@@ -13,15 +18,17 @@ def main():
     print("final result\n")
     print(np.around(result.final_state_vector, 3))
 
+    qft_circuit.append(cirq.measure(a,b,c,d, key='result'))
+    samples = simulator.run(qft_circuit,repetitions = 1000)
+
+    cirq.plot_state_histogram(samples, plt.subplot())
+    plt.show()
+
 def cz_and_swap(q0,q1,rot):
     yield cirq.CZ(q0,q1)**rot
     yield cirq.SWAP(q0,q1)
 
-def generate_2x2_grid_qft_circuit():
-    a,b,c,d = [cirq.GridQubit(0,0),
-              cirq.GridQubit(0,1),
-              cirq.GridQubit(1,1),
-              cirq.GridQubit(1,0),]
+def generate_2x2_grid_qft_circuit(a,b,c,d):
 
     circuit = cirq.Circuit(
         cirq.H(a),
